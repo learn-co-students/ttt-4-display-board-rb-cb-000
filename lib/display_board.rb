3a7979d2f2c934c46193a453275ecd939ce06a79
Jorge Cossi <jorge_cossi@yahoo.com>
2016-11-18 22:15:39 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,7 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+def display_board(args)
+  puts " #{args[0]} | #{args[1]} | #{args[2]} "
+  puts "-----------"
+  puts " #{args[3]} | #{args[4]} | #{args[5]} "
+  puts "-----------"
+  puts " #{args[6]} | #{args[7]} | #{args[8]} "
+end
