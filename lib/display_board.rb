796941d62459e64196ddf4b5f0a4a2d070156110
Derek Kaknes <derek.kaknes@gmail.com>
2016-12-08 23:46:16 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,15 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def fill_row(row)
+  " #{row[0]} | #{row[1]} | #{row[2]} \n"
+end
+
+def display_board(board)
+  sep = "-" * 11 + "\n"
+  res = fill_row(board[0..2])
+  res += sep
+  res += fill_row(board[3..5])
+  res += sep
+  res += fill_row(board[6..8])
+  puts res
+end
