662e99bc6a46be86ab42836f3bb1e8b9a18c44e7
Shuo Wang <gken90@gmail.com>
2016-12-29 01:39:17 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,12 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+def display_board(board)
+
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+board = ["X ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
+board[1] = " X "
+# puts board[1] = "X"   # board[0]="X"  #there should be 9 Strings
+display_board(board)
