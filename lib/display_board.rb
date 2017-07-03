e04890338e30f283e0029b4e10bdb5487c1b2dd8
David  Adegbesan <hiseholuwa@outlook.com>
2017-07-03 23:31:16 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,20 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board = [" "," "," "," "," "," "," "," "," "])
+  cell_one = " #{board[0]} "
+  cell_two = " #{board[1]} "
+  cell_three = " #{board[2]} "
+  cell_four = " #{board[3]} "
+  cell_five= " #{board[4]} "
+  cell_six = " #{board[5]} "
+  cell_seven = " #{board[6]} "
+  cell_eight = " #{board[7]} "
+  cell_nine = " #{board[8]} "
+  pole = "|"
+  dashes = "-----------"
+  puts "#{cell_one}#{pole}#{cell_two}#{pole}#{cell_three}"
+  puts "#{dashes}"
+  puts "#{cell_four}#{pole}#{cell_five}#{pole}#{cell_six}"
+  puts "#{dashes}"
+  puts "#{cell_seven}#{pole}#{cell_eight}#{pole}#{cell_nine}"
+end
