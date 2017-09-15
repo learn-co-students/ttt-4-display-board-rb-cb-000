d83594c430a6e56f0b8a10af120cb822189a01c8
Chris Ranni <cranni@marx.tech>
2017-09-15 03:01:12 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,14 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+ def display_board (layout)
+  pipe = "|"
+  dash = "-----------"
+  empty="   "
+
+  puts " #{layout[0]} #{pipe} #{layout[1]} #{pipe} #{layout[2]} "
+  puts "#{dash}"
+  puts " #{layout[3]} #{pipe} #{layout[4]} #{pipe} #{layout[5]} "
+  puts "#{dash}"
+  puts " #{layout[6]} #{pipe} #{layout[7]} #{pipe} #{layout[8]} "
+
+end
