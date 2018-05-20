e24189b9b82160b284c2d9781575e7a90e7c66ea
charles Bellorini <charlesbellorini@gmail.com>
2018-05-20 12:03:01 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,16 @@
 # Define display_board that accepts a board and prints
+def display_board (board)
+puts " #{board[0]} | #{board[1]} | #{board[2]} \n-----------\n #{board[3]} | #{board[4]} | #{board[5]} \n-----------\n #{board[6]} | #{board[7]} | #{board[8]} "
+end
 # out the current state.
+
+
+display_board([" ", " ", " ", " ", " ", " ", " ", " ", " "])
+display_board(["O", " ", " ", " ", " ", " ", " ", " ", " "])
+display_board(["O", " ", " ", " ", "X", " ", " ", " ", " "])
+display_board(["X", "X", "X", " ", " ", " ", " ", " ", " "])
+display_board([" ", " ", " ", " ", " ", " ", "O", "O", "O"])
+display_board(["X", " ", " ", " ", "X", " ", " ", " ", "X"])
+display_board([" ", " ", "O", " ", "O", " ", "O", " ", " "])
+display_board(["X", "X", "X", "X", "O", "O", "X", "O", "O"])
+display_board(["X", "O", "X", "O", "X", "X", "O", "X", "O"])
