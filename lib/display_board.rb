1647785e7fb1147fe22458161a1ecbdaeb6dabe7
Kathleen Howard <kmaryhoward@gmail.com>
2018-08-15 21:44:21 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,7 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+def display_board(board)
+  row_1 = " #{board[0]} | #{board[1]} | #{board[2]} "
+  row_2 = " #{board[3]} | #{board[4]} | #{board[5]} "
+  row_3 = " #{board[6]} | #{board[7]} | #{board[8]} "
+  line = "-----------"
+  print row_1 + "\n" + line + "\n"+ row_2 + "\n" + line + "\n" + row_3
+end
